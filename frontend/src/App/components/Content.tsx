4c0a1f0e0f993c00f0f8a7238003135a0efc248b
陈若枫 <768047505@qq.com>
2020-10-16 00:25:15 +0800

    frontend: provisional commit

--- frontend/src/App/components/Content.tsx
+++ frontend/src/App/components/Content.tsx
@@ -3,9 +3,11 @@
  */
 
 import React from 'react';
-import {Divider} from 'antd';
+import {Switch, Route} from 'react-router-dom'
 import {NavStyle} from '../index';
 import './index.less';
+
+const Home = React.lazy(() => import('@/pages/Home'));
 
 export type ContentProps = {
   navStyle: NavStyle
@@ -14,7 +16,11 @@ export type ContentProps = {
 const Content: React.FC<ContentProps> = ({navStyle}) => {
   return (
     <div className={`content${navStyle === 'mini' ? ' content-small' : ''}`}>
-      <Divider className="content-divider" plain>各大媒体平台热搜榜</Divider>
+      <React.Suspense fallback={<div>loading</div>}>
+        <Switch>
+          <Route path="/home" component={Home}></Route>
+        </Switch>
+      </React.Suspense>
     </div>
   );
 };
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+/**
+ * @file 路由-首页
+ */
+
+import React from 'react';
+import {Divider} from 'antd';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Divider className="content-divider" plain>各大平台热搜榜</Divider>
+    </div>
+  );
+};
+
+export default Home;
